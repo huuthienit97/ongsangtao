0bfc038dc8029dfe0faeae48dfb8ba2d0e65909b
huuthienit97 <huuthienit.2018@gmail.com>
2024-08-14 21:25:38 +0700

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://huuthientech:Thien1997@ongsangtao.rhfsi.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
