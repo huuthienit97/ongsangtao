dee57acdf7e125d7e86329b478f33b1a6a055fe2
huuthienit97 <huuthienit.2018@gmail.com>
2024-08-15 21:42:33 +0700

    config env, connect database & resource users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://huuthientech:Thien1997@ongsangtao.rhfsi.mongodb.net/')],
+  imports: [
+    //MongooseModule.forRoot('mongodb+srv://huuthientech:Thien1997@ongsangtao.rhfsi.mongodb.net/'),
+    MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      uri: configService.get<string>('MONGODB_URL'),
+    }),
+    inject: [ConfigService],
+  }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
